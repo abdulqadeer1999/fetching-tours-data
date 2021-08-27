190bb4b7336c1f77ef273cf64a86d733b7f2b741
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-08-27 23:17:30 +0500

    fetching tours data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+
+// import  List  from "./components-birth/List"
+// import { useState } from 'react';
+// import data from "./components-birth/data"
+
+
+
+// // Birth Day 
+// function App() {
+
+//   const [people, setPeople] = useState(data)
+
+//   const clear = () => {
+//     setPeople([])
+//   }
+//     return (
+//     <div className="App">
+//       <h1>{people.length} BirthDay Today</h1>
+//       <List people={people} />
+//       <button onClick={clear}>Clear All</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Tours
+
+
+import { useEffect, useState} from "react";
+import { Loading } from "./tour-compo/Loading";
+import { Tours } from "./tour-compo/Tours"
 import './App.css';
 
-function App() {
+const url = 'https://course-api.com/react-tours-project'
+
+const App = () => {
+
+  const [loading, setLoading] = useState(true)
+  const [tours,setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours)
+  }
+
+const fetchTours = async () => {
+  setLoading(true);
+  
+  try {
+    const response = await fetch(url);
+    const tours = await response.json();
+    setLoading(false)
+    setTours(tours)
+  } catch (error) {
+  setLoading(false)
+  console.log(error)
+  }
+
+  
+  
+}
+
+useEffect (() => {
+ fetchTours()
+},[])
+
+  if(loading) {
+    return (
+      <main>
+      <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>No Tours Left</h2>
+        <button className="btn" onClick={fetchTours}>Refresh</button>
+      </div>
+    </main>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+     <Tours tours={tours} removeTour={removeTour} />
+     </div>
+  )
 }
 
 export default App;
+
