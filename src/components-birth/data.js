763049dd7b18b8c99d1d10df95a1c1c3166f2267
abdulqadeer1999 <abdulqadeersolangi009@gmail.com>
2021-09-01 00:47:00 +0500

    changes

--- src/components-birth/data.js
+++ src/components-birth/data.js
@@ -1,36 +1,36 @@
 
-const data = [
-    {
-        id : 1,
-        name : "Abdul",
-        age : 23,
-        image : "https://img.freepik.com/free-photo/happy-optimistic-handsome-latin-sales-manager-pointing-aside-looking-camera_1262-12679.jpg?size=626&ext=jpg"
-    },
-    {
-        id : 2,
-        name : "AQ",
-        age : 27,
-        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9GNhmwl1xOGJ7msn-_AdrMJYOdKwy2grJDg&usqp=CAU"
-    },
-    {
-        id : 3,
-        name : "AS",
-        age : 20,
-        image : "https://thumbs.dreamstime.com/b/close-up-portrait-attractive-good-looking-man-show-thumb-s-stand-isolated-light-gray-background-look-camera-make-white-143136502.jpg"
-    },
-    {
-        id : 4,
-        name : "Abdul",
-        age : 30,
-        image : "https://image.shutterstock.com/image-photo/portrait-young-man-crossed-hands-260nw-582575041.jpg"
-    },
-    {
-        id : 5,
-        name : "Anas",
-        age : 27,
-        image : "https://media.istockphoto.com/photos/portrait-of-a-smiling-student-at-the-city-street-picture-id1147289240?k=6&m=1147289240&s=612x612&w=0&h=EBUqpCRQz1s3ssDSuF0JBT0_22copoJKbZ7FNOxHr7c="
-    },
+// const data = [
+//     {
+//         id : 1,
+//         name : "Abdul",
+//         age : 23,
+//         image : "https://img.freepik.com/free-photo/happy-optimistic-handsome-latin-sales-manager-pointing-aside-looking-camera_1262-12679.jpg?size=626&ext=jpg"
+//     },
+//     {
+//         id : 2,
+//         name : "AQ",
+//         age : 27,
+//         image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9GNhmwl1xOGJ7msn-_AdrMJYOdKwy2grJDg&usqp=CAU"
+//     },
+//     {
+//         id : 3,
+//         name : "AS",
+//         age : 20,
+//         image : "https://thumbs.dreamstime.com/b/close-up-portrait-attractive-good-looking-man-show-thumb-s-stand-isolated-light-gray-background-look-camera-make-white-143136502.jpg"
+//     },
+//     {
+//         id : 4,
+//         name : "Abdul",
+//         age : 30,
+//         image : "https://image.shutterstock.com/image-photo/portrait-young-man-crossed-hands-260nw-582575041.jpg"
+//     },
+//     {
+//         id : 5,
+//         name : "Anas",
+//         age : 27,
+//         image : "https://media.istockphoto.com/photos/portrait-of-a-smiling-student-at-the-city-street-picture-id1147289240?k=6&m=1147289240&s=612x612&w=0&h=EBUqpCRQz1s3ssDSuF0JBT0_22copoJKbZ7FNOxHr7c="
+//     },
     
-]
+// ]
 
-export default data;
+// export default data;
